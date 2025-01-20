846aa98b974a42fe039a938b5726e80f2101d933
tihosashka <tihonenkoav@yandex.ru>
2025-01-20 19:50:41 +0300

    lab2 v1

---
+++ src/lab2/Colour.java
@@ -0,0 +1,20 @@
+package lab2;
+
+public enum Colour {
+    Black(""),
+    White(""),
+    Grey("Серый"),
+    Orange(""),
+    Green(""),
+    Blue("");
+
+    private final String description;
+
+    Colour(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
